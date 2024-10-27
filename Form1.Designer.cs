8af397af29d66b8570595059c62c8a37f9fc32e8
NguyenKhoa.git <160829826+NguynK04@users.noreply.github.com>
2024-10-27 20:45:03 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Databinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btthem = new System.Windows.Forms.Button();
+            this.btsua = new System.Windows.Forms.Button();
+            this.btxoa = new System.Windows.Forms.Button();
+            this.lblfullname = new System.Windows.Forms.Label();
+            this.lblage = new System.Windows.Forms.Label();
+            this.lblnganhhoc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 52);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(140, 111);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(330, 52);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(558, 263);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "CNTT",
+            "Quan Tri Kinh Doanh"});
+            this.comboBox1.Location = new System.Drawing.Point(140, 175);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(136, 24);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // btthem
+            // 
+            this.btthem.Location = new System.Drawing.Point(30, 286);
+            this.btthem.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btthem.Name = "btthem";
+            this.btthem.Size = new System.Drawing.Size(67, 32);
+            this.btthem.TabIndex = 4;
+            this.btthem.Text = "Thêm";
+            this.btthem.UseVisualStyleBackColor = true;
+            this.btthem.Click += new System.EventHandler(this.btthem_Click);
+            // 
+            // btsua
+            // 
+            this.btsua.Location = new System.Drawing.Point(128, 286);
+            this.btsua.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btsua.Name = "btsua";
+            this.btsua.Size = new System.Drawing.Size(67, 32);
+            this.btsua.TabIndex = 5;
+            this.btsua.Text = "Sửa";
+            this.btsua.UseVisualStyleBackColor = true;
+            this.btsua.Click += new System.EventHandler(this.btsua_Click);
+            // 
+            // btxoa
+            // 
+            this.btxoa.Location = new System.Drawing.Point(224, 286);
+            this.btxoa.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btxoa.Name = "btxoa";
+            this.btxoa.Size = new System.Drawing.Size(67, 32);
+            this.btxoa.TabIndex = 6;
+            this.btxoa.Text = "Xóa";
+            this.btxoa.UseVisualStyleBackColor = true;
+            this.btxoa.Click += new System.EventHandler(this.btxoa_Click);
+            // 
+            // lblfullname
+            // 
+            this.lblfullname.AutoSize = true;
+            this.lblfullname.Location = new System.Drawing.Point(27, 52);
+            this.lblfullname.Name = "lblfullname";
+            this.lblfullname.Size = new System.Drawing.Size(52, 16);
+            this.lblfullname.TabIndex = 7;
+            this.lblfullname.Text = "Ho Ten";
+            // 
+            // lblage
+            // 
+            this.lblage.AutoSize = true;
+            this.lblage.Location = new System.Drawing.Point(27, 116);
+            this.lblage.Name = "lblage";
+            this.lblage.Size = new System.Drawing.Size(34, 16);
+            this.lblage.TabIndex = 8;
+            this.lblage.Text = "Tuoi";
+            // 
+            // lblnganhhoc
+            // 
+            this.lblnganhhoc.AutoSize = true;
+            this.lblnganhhoc.Location = new System.Drawing.Point(27, 182);
+            this.lblnganhhoc.Name = "lblnganhhoc";
+            this.lblnganhhoc.Size = new System.Drawing.Size(72, 16);
+            this.lblnganhhoc.TabIndex = 9;
+            this.lblnganhhoc.Text = "Nganh hoc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(416, 319);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 33);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 319);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Previous";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 518);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblnganhhoc);
+            this.Controls.Add(this.lblage);
+            this.Controls.Add(this.lblfullname);
+            this.Controls.Add(this.btxoa);
+            this.Controls.Add(this.btsua);
+            this.Controls.Add(this.btthem);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Quản lý thông tin sinh viên";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btthem;
+        private System.Windows.Forms.Button btsua;
+        private System.Windows.Forms.Button btxoa;
+        private System.Windows.Forms.Label lblfullname;
+        private System.Windows.Forms.Label lblage;
+        private System.Windows.Forms.Label lblnganhhoc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using Databinding.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Databinding
+{
+    public partial class Form1 : Form
+    {
+        private StudentModel studentModel = new StudentModel();
+        private BindingSource bindingSource = new BindingSource();
+        private int currentIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            bindingSource.DataSource = studentModel.Students.ToList();
+            dataGridView1.DataSource = bindingSource;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       // private void Form1_Load(object sender, EventArgs e)
+       // {
+
+       // }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btthem_Click(object sender, EventArgs e)
+        {
+            var student = new Students
+            {
+                FullName = textBox1.Text,
+                Age = int.Parse(textBox2.Text),
+                Major = comboBox1.Text
+            };
+            studentModel.Students.Add(student);
+            studentModel.SaveChanges();
+
+            bindingSource.DataSource = studentModel.Students.ToList();
+        }
+
+        private void btsua_Click(object sender, EventArgs e)
+        {
+            if (bindingSource.Current is Students student)
+            {
+                student.FullName = textBox1.Text;
+                student.Age = int.Parse(textBox2.Text);
+                student.Major = comboBox1.Text;
+                studentModel.SaveChanges();
+
+                bindingSource.DataSource = studentModel.Students.ToList();
+            }
+        }
+
+        private void btxoa_Click(object sender, EventArgs e)
+        {
+           
+                if (bindingSource.Current is Students student)
+                {
+                    studentModel.Students.Remove(student);
+                    studentModel.SaveChanges();
+                    bindingSource.DataSource = studentModel.Students.ToList();
+                }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (currentIndex < bindingSource.Count - 1)
+            {
+                currentIndex++;
+                bindingSource.Position = currentIndex;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (currentIndex > 0)
+            {
+                currentIndex--;
+                bindingSource.Position = currentIndex;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (bindingSource.Current is Students student)
+            {
+                textBox1.Text = student.FullName;
+                textBox2.Text = student.Age.ToString();
+                comboBox1.Text = student.Major;
+            }
+        }
+    }
+}
